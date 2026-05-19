14f9b5033f3f223dd95687f8af6cdd150bc1858a
Waycan Admin <admin@waycan.com>
2026-05-19 20:08:45 +0300

    chore: configure next.config.js for Supabase remote patterns

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'picsum.photos'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
   },
 }
 
